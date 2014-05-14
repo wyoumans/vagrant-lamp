ce790d70eb5921d99b138954962551156912e95e
William Youmans <will@williamyoumans.com>
2014-05-14 08:55:52 -0600

    whitespace

--- cookbooks/vagrant_main/recipes/wordpress.rb
+++ cookbooks/vagrant_main/recipes/wordpress.rb
@@ -4,7 +4,7 @@ include_recipe "python"
 python_pip "wp2github"
 
 # Install wp-cli
-remote_file "/usr/local/bin/wp" do                                              
+remote_file "/usr/local/bin/wp" do
   source 'https://raw.github.com/wp-cli/builds/gh-pages/phar/wp-cli.phar'
   mode 0755
   action :create_if_missing
